9d86f8c4f2bd09a5d82c8b9599a6939804d9ad50
pwjworks <13610513466@163.com>
2022-02-17 04:39:20 +0800

    test & configure

--- main.cpp
+++ main.cpp
@@ -1,10 +1,51 @@
 #include "tinyplayer.h"
+#include"XDemux.h"
+#include"XDecode.h"
 #include <QtWidgets/QApplication>
+#include <iostream>
+extern "C" {
+#include <libavcodec/avcodec.h>
+}
 
-int main(int argc, char *argv[])
+using namespace std;
+
+int main(int argc, char* argv[])
 {
-    QApplication a(argc, argv);
-    TinyPlayer w;
-    w.show();
-    return a.exec();
+	XDemux demux;
+	cout << "demux.open = " << demux.Open("test.mp4");
+
+	demux.Clear();
+	demux.Close();
+	cout << "demux.Open = " << demux.Open("test.mp4");
+	cout << "CopyVPara: " << demux.CopyVPara() << endl;
+	cout << "CopyAPara: " << demux.CopyAPara() << endl;
+	cout << "seed = " << demux.Seek(0.5) << endl;
+
+	XDecode vdecoder;
+	cout << "vdecoder.Open =" << vdecoder.Open(demux.CopyVPara()) << endl;
+	//vdecoder.Clear();
+	//vdecoder.Close();
+	XDecode adecoder;
+	cout << "adecoder.Open =" << adecoder.Open(demux.CopyAPara()) << endl;
+	for (;;) {
+		AVPacket* pkt = demux.Read();
+		if (demux.IsAudio(pkt)) {
+			adecoder.Send(pkt);
+			AVFrame* frame = adecoder.Recv();
+			cout << "Audio: " << frame << endl;
+			av_frame_free(&frame);
+		}
+		else {
+			vdecoder.Send(pkt);
+			AVFrame* frame = vdecoder.Recv();
+			cout << "Video: " << frame << endl;
+			av_frame_free(&frame);
+		}
+		if (!pkt) break;
+	}
+
+	QApplication a(argc, argv);
+	TinyPlayer w;
+	w.show();
+	return a.exec();
 }
