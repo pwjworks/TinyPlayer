ecb7a37d74e7f7e5d00dc8c2a2e13df2fbcaa878
pwjworks <13610513466@163.com>
2022-02-17 04:38:44 +0800

    video decode

---
+++ XDecode.cpp
@@ -0,0 +1,86 @@
+#include "XDecode.h"
+#include<iostream>
+extern "C" {
+#include <libavcodec/avcodec.h>
+}
+
+
+void XDecode::Close() {
+	std::lock_guard<std::mutex> lck(mux_);
+	if (codecContext) {
+		avcodec_close(codecContext);
+		avcodec_free_context(&codecContext);
+	}
+}
+void XDecode::Clear() {
+	std::lock_guard<std::mutex> lck(mux_);
+	// 清理解码缓冲
+	if (codecContext) avcodec_flush_buffers(codecContext);
+
+}
+
+bool XDecode::Open(AVCodecParameters* para) {
+	if (!para) return false;
+	// 打开视频解码器
+	AVCodec* codec = avcodec_find_decoder(para->codec_id);
+	if (!codec) {
+		avcodec_parameters_free(&para);
+		std::cout << "can't find the codec id " << para->codec_id << std::endl;
+		return false;
+	}
+	std::cout << "codec found! " << para->codec_id << std::endl;
+
+	// 加锁，保证codecContext访问互斥
+	std::unique_lock<std::mutex> lck(mux_, std::try_to_lock);
+	if (!lck.owns_lock()) return false;
+
+	// 构建解码器上下文
+	codecContext = avcodec_alloc_context3(codec);
+
+	// 配置解码器上下文参数
+	avcodec_parameters_to_context(codecContext, para);
+	codecContext->thread_count = 8;
+
+	int ret = avcodec_open2(codecContext, nullptr, nullptr);
+	if (ret != 0) {
+		avcodec_free_context(&codecContext);
+		lck.unlock();
+		char buf[1024] = { 0 };
+		av_strerror(ret, buf, sizeof(buf) - 1);
+		std::cout << "avcodec_open2 failed! :" << buf << std::endl;
+		return false;
+	}
+	avcodec_parameters_free(&para);
+	return true;
+}
+
+bool XDecode::Send(AVPacket* pkt) {
+	if (!pkt || pkt->size <= 0 || !pkt->data) return false;
+	{
+		std::lock_guard<std::mutex> lck(mux_);
+		if (!codecContext) return false;
+		int ret = avcodec_send_packet(codecContext, pkt);
+		// 释放空间
+		av_packet_free(&pkt);
+		if (ret != 0)return false;
+	}
+	return true;
+}
+
+AVFrame* XDecode::Recv() {
+	std::lock_guard<std::mutex> lck(mux_);
+	if (!codecContext) return nullptr;
+	AVFrame* frame = av_frame_alloc();
+	int re = avcodec_receive_frame(codecContext, frame);
+	if (re != 0) {
+		av_frame_free(&frame);
+		return nullptr;
+	}
+	std::cout << "[" << frame->linesize[0] << "] " << std::flush;
+	return frame;
+}
+
+XDecode::XDecode() {};
+
+XDecode::~XDecode() {};
+
---
+++ XDecode.h
@@ -0,0 +1,30 @@
+#pragma once
+#include<mutex>
+
+struct AVCodecParameters;
+struct AVCodecContext;
+struct AVFrame;
+struct AVPacket;
+
+class XDecode {
+public:
+	bool isAudio = false;
+	// 打开解码器，不管成功与否都释放para空间。
+	virtual bool Open(AVCodecParameters* para);
+
+	// 发送到解码线程，不管成功与否都释放pkt空间（对象和媒体内容）
+	virtual bool Send(AVPacket* pkt);
+
+	//获取解码数据，一次send可能需要多次Recv，获取缓冲中的数据Send NULL在Recv多次
+	//每次复制一份，由调用者释放 av_frame_free
+	virtual AVFrame* Recv();
+
+	virtual void Close();
+	virtual void Clear();
+	XDecode();
+	~XDecode();
+private:
+	AVCodecContext* codecContext = 0;
+	std::mutex mux_;
+};
+
